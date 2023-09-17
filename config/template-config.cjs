463e383c3e400fe78c99a9cebc21ae83213f91e8
翁文辉 <74484047+H-weng89@users.noreply.github.com>
2023-09-17 20:55:41 +0800

    Update template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -2,31 +2,36 @@
 const TEMPLATE_CONFIG = [
   {
     id: '0001',
-    title: '宝宝, 早上好',
-    desc: `
-      今天是:{{date.DATA}}
-      今天是我们恋爱的第{{love_day.DATA}}天 
-      下个休息日：{{holidaytts.DATA}}
-      今天第一节课程:{{wx_course_schedule_0.DATA}} 
-      ---
-      
-      今天{{city.DATA}}的天气如下:
-      
-      天气：{{tian_api_weather_weather_0.DATA}}
-      
-      气温:{{tian_api_weather_lowest_0.DATA}}-{{tian_api_weather_highest_{offset}.DATA}}
-      日出时间:{{tian_api_weather_sunrise_0.DATA}}
-      日落时间:{{tian_api_weather_sunset_0.DATA}}
-      每天一言:{{tian_api_morning_greeting.DATA}}
-      推送人:小翁
+title: '宝宝, 早上好',
+desc: `
+  今天是:{{date.DATA}}
 
-      
-      ---
-      
-      {{moment_copyrighting.DATA}}
-      
-      {{poetry_title.DATA}} {{poetry_content.DATA}}
-    `
+  今天是我们恋爱的第{{love_day.DATA}}天 
+
+  今天第一节课程:{{wx_course_schedule_0.DATA}} 
+  ---
+  
+  今天{{city.DATA}}的天气如下:
+  
+  天气：{{tian_api_weather_weather_0.DATA}}
+  
+  气温:{{tian_api_weather_lowest_0.DATA}}-{{tian_api_weather_highest_0.DATA}}
+
+  日出时间:{{tian_api_weather_sunrise_0.DATA}}
+
+  日落时间:{{tian_api_weather_sunset_0.DATA}}
+
+  每天一言:{{tian_api_morning_greeting.DATA}}
+
+  推送人:小翁
+
+  
+  ---
+  
+  {{moment_copyrighting.DATA}}
+  
+  {{poetry_title.DATA}} {{poetry_content.DATA}}
+`
   },
   {
     id: '0002',
